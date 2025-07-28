8afe77d9d04f5bf8e2c764d20237ac81e54e0029
Tania Sharma <tanushrma1022@gmail.com>
2025-07-28 13:38:48 +0530

    Add all project files

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const fs = require("fs");
+
+//file ko show krne wala part
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  fs.readdir(`./files`, function (err, files) {
+    if (err) {
+      console.error("Failed to read directory:", err);
+      return res.render("index", { files: [] });
+    }
+    res.render("index", { files: files });
+  });
+});
+
+app.get("/file/:filename", function (req, res) {
+  fs.readFile(
+    `./files/${req.params.filename}`,
+    "utf-8",
+    function (err, filedata) {
+      res.render("show", { filename: req.params.filename, filedata: filedata });
+    }
+  );
+});
+
+app.get("/edit/:filename", function (req, res) {
+  res.render("edit", { filename: req.params.filename });
+});
+
+app.post("/edit", function (req, res) {
+  fs.rename(
+    `./files/${req.body.previous}`,
+    `./files/${req.body.new}`,
+    function (err) {}
+  );
+});
+
+app.post("/create", function (req, res) {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.details,
+    function (err) {}
+  );
+  res.redirect("/");
+});
+
+app.listen(3000, function () {
+  console.log("Its running");
+});
